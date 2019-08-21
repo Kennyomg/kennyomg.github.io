75a1dbfe137ededf105f7125421e1a8e2ea9505c
Kenrick Halff <kenrickhalff@gmail.com>
2019-08-21 21:11:12 +0200

    Initial Demo build

---
+++ precache-manifest.4100f388c61fa4726160b925602de14c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e94d8dbbdec958d696d0",
+    "url": "0.js"
+  },
+  {
+    "revision": "f3c1d49781d9a113126f",
+    "url": "main.js"
+  },
+  {
+    "revision": "ef07d26ac3c9e211b8a5",
+    "url": "2.js"
+  },
+  {
+    "revision": "0c7fcf38103d81254489",
+    "url": "3.js"
+  },
+  {
+    "revision": "4d9c5905193ccc66026c",
+    "url": "4.js"
+  },
+  {
+    "revision": "9fc652aba7e46076be51",
+    "url": "5.js"
+  },
+  {
+    "revision": "03b610589aee2dc4ffc2",
+    "url": "6.js"
+  },
+  {
+    "revision": "ae75edb01ffae3b75eaa",
+    "url": "7.js"
+  },
+  {
+    "revision": "38da20c76c17347485ba",
+    "url": "8.js"
+  },
+  {
+    "revision": "1825c4a6c00158146c013368a6678e77",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "0ffe3f59445b828f6f742c334b76a112",
+    "url": "index.html"
+  }
+];
